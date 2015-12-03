419ee4eec85bcb47f98eac5f070ca8d5f680b516
Scott Christensen <mscottchristensen@gmail.com>
2015-12-03 12:02:31 -0700

    initial commit

---
+++ csvQUICKmap/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace csvQUICKmap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.Y_Value_DropDown = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(564, 165);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 0;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(356, 165);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerate.TabIndex = 1;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // Y_Value_DropDown
+            // 
+            this.Y_Value_DropDown.FormattingEnabled = true;
+            this.Y_Value_DropDown.Items.AddRange(new object[] {
+            "MAP (psi)",
+            "TPS   (%)"});
+            this.Y_Value_DropDown.Location = new System.Drawing.Point(22, 25);
+            this.Y_Value_DropDown.Name = "Y_Value_DropDown";
+            this.Y_Value_DropDown.Size = new System.Drawing.Size(121, 21);
+            this.Y_Value_DropDown.TabIndex = 3;
+            this.Y_Value_DropDown.Text = "TPS   (%)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Y Values of Table";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "X Values of Table";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "RPM"});
+            this.comboBox1.Location = new System.Drawing.Point(22, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "RPM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cell Values of Table";
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.DataSource = this.form1BindingSource;
+            this.listBox1.DisplayMember = "AutoScroll";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(191, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(427, 108);
+            this.listBox1.TabIndex = 8;
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(csvQUICKmap.Form1);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Lambda"});
+            this.comboBox2.Location = new System.Drawing.Point(22, 105);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.Text = "Lambda";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(243, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(327, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Files to be averaged (drag and drop .csv output files from ECU here)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(462, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Remove";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 201);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Y_Value_DropDown);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.buttonCancel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "csvQUICKmap";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.ComboBox Y_Value_DropDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ csvQUICKmap/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csvQUICKmap
+{
+    public partial class Form1 : Form
+    {
+        //variables 
+        private BindingList<string> filenames = new BindingList<string>();
+        private double[,,] cells = new double[25,26,2];
+
+        //some form settings
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.AllowDrop = true;
+            listBox1.DragEnter += new DragEventHandler(listBox1_DragEnter);
+            listBox1.DragDrop += new DragEventHandler(listBox1_DragDrop);
+            listBox1.DataSource = filenames;
+        }
+
+        //events
+        void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                filenames.Add(file);
+            }
+            listBox1.Update();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            //create lists for each variable
+            List<double> rpm = new List<double>();
+            List<double> tps = new List<double>();
+            List<double> map = new List<double>();
+            List<double> lambda = new List<double>();
+
+            //populate lists with data
+            foreach (string filename in filenames)
+            {
+                StreamReader reader = new StreamReader(File.OpenRead(filename));
+                string line = reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    string[] values = line.Split(',');
+                    rpm.Add(Convert.ToDouble(values[1]));
+                    tps.Add(Convert.ToDouble(values[4]));
+                    map.Add(Convert.ToDouble(values[6]));
+                    lambda.Add(Convert.ToDouble(values[10]));
+                }
+                reader.Close();
+            }
+
+            //Generate map of data
+            for(int i = 0; i < lambda.Count; i++)
+            {
+                //casting as an int truncates, which is what we want I'm pretty sure.
+                int rpmIndex = (int)(rpm[i]/250);
+                int tpsIndex = (int)(tps[i]/4);
+                int mapIndex = (int)((map[i] - 2.5)/0.5);
+                if (Y_Value_DropDown.Text == "TPS   (%)")
+                {
+                    cells[rpmIndex, tpsIndex, 0] += 1;
+                    cells[rpmIndex, tpsIndex, 1] += lambda[i];
+                }
+                else
+                {
+                    cells[rpmIndex, tpsIndex, 0] += 1;
+                    cells[rpmIndex, tpsIndex, 1] += lambda[i];
+                }
+            }
+
+            //generate unique filepath that (current local time)
+            string outputFilePath = @"C:/Users\Mobile Wind Tunnel/Desktop/Scott's Tunes/Data Aquisition/convertedFiles/output.csv";
+            StringBuilder csv = new StringBuilder();
+
+            for(int i = 26; i >= 0; i--)
+            {
+                for(int j = 0; j < 25; j++)
+                {
+                    if (i == 26)
+                    {
+                        if(j == 0)
+                        {
+                            csv.Append(" ,");
+                        }
+                        if(j != 24)
+                        {
+                            csv.Append(string.Format("{0}, ", (j * 250).ToString()));
+                        }
+                        else if(j == 24)
+                        {
+                            csv.Append(string.Format("{0}{1} ", (j * 250).ToString(), Environment.NewLine));
+                        }
+                    }
+                    else
+                    {
+                        if (j == 0)
+                        {
+                            //add in the tps value column
+                            if(Y_Value_DropDown.Text == "TPS   (%)")
+                            {
+                                csv.Append(string.Format("{0}, ", (i*4).ToString()));
+                            }
+                            //add in the map value column
+                            else
+                            {
+                                csv.Append(string.Format("{0}, ", (2.5 + (i*0.5)).ToString()));
+                            }
+                        }
+                        if(j != 24)
+                        {
+                            if (cells[j, i, 0] != 0)
+                            {
+                                csv.Append(string.Format("{0}, ", (cells[j, i, 1] / cells[j, i, 0]).ToString()));
+                            }
+                            else
+                            {
+                                csv.Append("-1, ");
+                            }
+                        }
+                        else if (j == 24)
+                        {
+                            if (cells[j, i, 0] != 0)
+                            {
+                                csv.Append(string.Format("{0}{1}", (cells[j, i, 1] / cells[j, i, 0]).ToString(), Environment.NewLine));
+                            }
+                            else
+                            {
+                                csv.Append(string.Format("{0}{1}", "-1", Environment.NewLine));
+                            }
+                        }
+                    }
+                }
+            }
+
+            File.WriteAllText(outputFilePath.ToString(), csv.ToString());
+
+            MessageBox.Show(string.Format("File written to {0}",outputFilePath));
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            filenames.Remove(listBox1.SelectedItem.ToString());
+            listBox1.Update();
+        }
+    }
+
+}
